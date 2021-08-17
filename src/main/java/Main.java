f278191c3836a126f1f0ef1d577942dfdf924311
Jessica Yu <jessicayu62@gmail.com>
2021-08-16 20:14:39 -0700

    Update main and create launcher class for jar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,22 +6,37 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Objects;
+import java.io.IOException;
+
+import javafx.scene.layout.StackPane;
 
 public class Main extends Application {
 
+    private static StackPane pane = new StackPane();
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../resources/pomtimer.fxml")));
-        primaryStage.setTitle("Pomodoro Timer");
-        primaryStage.setScene(new Scene(root, 800, 630));
-        primaryStage.setMinHeight(600);
-        primaryStage.setMinWidth(750);
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("main/resources/pomtimer.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        pane.getChildren().add(root);
+
+        Scene scene = new Scene(pane);
+
+        stage.setTitle("Pomodoro Timer");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setFullScreen(false);
+        stage.show();
+
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         launch(args);
+        System.exit(1);
     }
 }
